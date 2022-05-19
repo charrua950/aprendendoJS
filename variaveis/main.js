4a96fa0a010ff64809cd5de187eb8b4d0058a4c9
charrua950 <jrcf90@gmail.com>
2022-05-19 07:01:42 -0300

    tipo de dados

---
+++ variaveis/main.js
@@ -0,0 +1,35 @@
+var myname = 'Juan'
+var age = 31
+var student = true
+
+console.log(myname, age, student)
+
+var largo = 10
+var ancho = 5
+var area = largo * ancho
+
+console.log(area)
+
+//sem valor
+
+var semDefinir
+console.log(semDefinir)
+
+//Variavel não pode começar assim:
+
+// var @Named;
+// var function;
+// var 1ola;
+
+
+//Variavel pode começar assim:
+
+var $selecionar;
+var _nome;
+var olaMundo;
+
+
+
+
+
+
